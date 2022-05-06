5e5f13eed2c950b943814a34bc937ea023275bec
Artur Sharifullin <artursh62@gmail.com>
2022-05-07 00:50:35 +0500

    'commit1'

---
+++ js/script.js
@@ -0,0 +1,108 @@
+'use strict'
+document.addEventListener('DOMContentLoaded', () => {
+	const form = document.querySelector('#form');
+	form.addEventListener('submit', formSend);
+	async function formSend(e) {
+		e.preventDefault();
+
+		let error = formValidate(form);
+
+		let formData = new FormData(form);
+		formData.append('image', formImage.files[0]);
+		if (error === 0) {
+			form.classList.add('_sending');
+			let response = await fetch('sendmail.php', {
+				method: 'POST',
+				body: formData
+			});
+			if (response.ok) {
+				let result = await response.json();
+				alert(result.message);
+				formPreview.innerHTML = '';
+				form.reset();
+				form.classList.remove('_sending');
+			} else {
+				alert('Ошибка');
+				form.classList.remove('_sending');
+			}
+		}
+		else {
+			alert('Заполните все поля!');
+		}
+	}
+
+
+	function formValidate(form) {
+		let error = 0;
+		let formReq = document.querySelectorAll('._req');
+		formReq.forEach((input, i) => {
+			formRemoveError(input);
+
+			if (input.classList.contains('_email')) {
+				if (emailTest(input)) {
+					formAddError(input);
+					error++;
+				}
+			}
+			else if (input.getAttribute('type') === 'checkbox' && (input.checked === false)) {
+				formAddError(input);
+				error++;
+			}
+			else if (input.value === '') {
+				formAddError(input);
+				error++;
+			}
+		});
+
+		return error;
+	}
+
+	function formAddError(input) {
+		input.parentElement.classList.add('_error');
+		input.classList.add('_error');
+	}
+
+	function formRemoveError(input) {
+		input.parentElement.classList.remove('_error');
+		input.classList.remove('_error');
+	}
+
+	function emailTest(input) {
+		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	}
+
+
+
+	const formImage = document.querySelector('#formImage');
+
+	const formPreview = document.querySelector('#formPreview');
+
+	formImage.addEventListener('change', () => {
+
+	});
+
+	function uploadFile(file) {
+		if (!['image/jpeg', 'image/png', 'image/gif'].includes(file.type)) {
+			alert('Разрещены только изображения. ');
+			formImage.value = '';
+			return;
+		}
+		if (file.size > 2 * 1024 * 1024) {
+			alert('Файл должен быть менее 2 МБ');
+			return;
+		}
+		let reader = new FileReader();
+		reader.onload = function (e) {
+			formPreview.innerHTML = `<img src="${e.target.result}" alt="Фото"/>`;
+		};
+		reader.onerror = function (e) {
+			alert('Ошибка');
+		};
+		reader.readAsDataURL(file);
+
+
+
+
+	}
+
+});
